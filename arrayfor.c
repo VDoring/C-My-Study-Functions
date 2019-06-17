11b56fc6e8ff34c7f72d96f292a1e27fe9325ab2
Doring <hojun.dev@gmail.com>
2019-06-17 10:24:01 +0900

    array + for

--- arrayfor.c
+++ arrayfor.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-int main() {
-	int num[5] = { 100,200,300,400,500 };
-
-	for (int i = 0; i < sizeof(num) / sizeof(int); i++) {
-		printf("%d\n", num[i]); //num[i]는 num[1]부터 num[5]까지 출력하게 되어있다.
+int main()
+{
+	int number[4];
+	
+	for (int i = 0; i < 4; i++) {
+		printf("number[%d]를 입력하십시오 : ", i);
+		scanf("%d", &number[i]);
 	}
+
+	printf("\n");
+
+	for (int i = 0; i < 4; i++)
+		printf("number[%d]는 %d 입니다\n", i, number[i]);
+
+
 	return 0;
 }
