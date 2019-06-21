e528a9a5ef05a0246a7d40019649ca9c26a3734e
Doring <hojun.dev@gmail.com>
2019-06-21 18:44:51 +0900

    header series 3

---
+++ hd.h
@@ -0,0 +1,15 @@
+int sum(int a, int b);
+
+
+
+int a = 5, b = 10;
+
+
+
+int sum(int a, int b)
+
+{
+
+	return a + b;
+
+}
---
+++ mainer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "hd.h"
+
+
+void main()
+
+{
+
+	printf("%d + %d = %d", a, b, sum(a, b));
+
+}
