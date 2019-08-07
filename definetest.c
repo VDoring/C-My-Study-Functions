a7f244b4faf93f527ebe6741ad21d250e58b2012
Doring <hojun.dev@gmail.com>
2019-08-07 20:31:16 +0900

    define basic

---
+++ definetest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define A "ABCD!"
+#define X x * x
+#define Mi x - x
+int x = 2;
+
+int main()
+{
+	printf("Hello\n");
+	printf(A);
+	printf(X);
+}
+
+// http://sarghis.com/blog/802/
