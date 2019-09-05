7ff79919a0c29ea206a3d22eac13bc24fbb2ea13
Doring <hojun.dev@gmail.com>
2019-09-05 18:18:34 +0900

    Usage fgets()
    a.txt라는 파일이 있고, 데이터가 저장되어있다는 가정하에 동작가능
    
    char from_a_txt[30];
    char from_a_txt2[30];
    
    FILE *file_pointer;
    file_pointer=fopen("a.txt", "r");
    
    fscanf(file_pointer, "%s" from_a_txt);
    printf("읽어온 부분 : %s \n", from_a_txt);
    
    fgets(from_a_txt2, 30, file_pointer);
    printf("읽어온 부분 : %s \n", from_a_txt2);
    
    fclose(file_pointer);

---
+++ fgets.c
@@ -0,0 +1,27 @@
+/* fgets */
+
+#include <stdio.h>
+
+int main()
+
+{
+
+char from_a_txt[30];
+char from_a_txt2[30];
+
+FILE *file_pointer;
+file_pointer=fopen("a.txt", "r");
+
+fscanf(file_pointer, "%s" from_a_txt);
+printf("읽어온 부분 : %s \n", from_a_txt);
+
+fgets(from_a_txt2, 30, file_pointer);
+printf("읽어온 부분 : %s \n", from_a_txt2);
+
+fclose(file_pointer);
+
+return 0;
+
+}
+
+// https://desire-with-passion.tistory.com/169
