c24e4ee33eeafe001d6100ca9f04de95b9b2a717
Doring <hojun.dev@gmail.com>
2019-12-18 16:22:11 +0900

    If you write "=" instead of "==" in if()
    if문 안에서 값을 할당하게 된다.
    즉 0이 아닌 숫자는 참(1)으로 처리되어 if문이 실행되기에 혼동하지 않도록 주의해야한다.

---
+++ conditional_and_allocation_operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int num = 5;
+
+	if (num = 10) // if문 안에서 num에 10을 할당. 0이 아닌 숫자므로 10은 참이다
+		printf("10입니다\n");
+
+	return 0;
+}
+// https://dojang.io/mod/page/view.php?id=130
