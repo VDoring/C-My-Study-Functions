796be2f3d160686871c3c7153c694c8a8dcc3317
Doring <hojun.dev@gmail.com>
2020-03-02 22:32:46 +0900

    Ternary operator printf() usage

---
+++ ternary_operator_in_printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int num1 = 5;
+
+	printf("%s\n", num1 == 10 ? "10입니다" : "10이 아닙니다");
+	//num1은 5이므로 "10이 아닙니다" 출력
+
+	return 0;
+}
+
+// https://dojang.io/mod/page/view.php?id=147
