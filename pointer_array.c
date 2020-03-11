82b195a2dd4e7cce73a0e77578c24dc3695e88e0
Doring <hojun.dev@gmail.com>
2020-03-11 11:15:55 +0900

    Pointer array basic

--- pointer_array.c
+++ pointer_array.c
@@ -2,11 +2,18 @@
 
 int main() {
 	//char text[5] = { "N", "O", "P", "W", "R" };
-	char *text[5] = { "Power", "OverClock", "Watt", "Electric", "Reverse" };
+	char *text[5] = { "Power", "OverClock", "Watt", "Electric", "Reverse" }; //%s를 사용
+	//char text[5] = { 'H', 'E', 'L', 'L', 'O'};  //%c를 사용
 
 	for (int i = 0; i < 5; i++) {
 		printf("%s\n", text[i]);
 	}
+
+	/*
+	for (int i = 0; i < 5; i++) {
+		printf("%c\n", text[i]);
+	}*/
+
 	printf("\n이것이 포인터배열의 힘!");
 	printf("\n그냥 배열은 이렇게 여러개 못 출력하지. 하하하! 보았나 이 *별*의 힘을 말이다!!\n\n");
 	return 0;
